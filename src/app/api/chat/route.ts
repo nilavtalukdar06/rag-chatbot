0fd16085a70862bc7a81f72e413615a0a7a30b29
Web <webd75369@gmail.com>
2026-06-29 22:57:00 +0530

    updated messages route to persist messages

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,10 +2,12 @@ import { streamText, UIMessage, convertToModelMessages } from "ai";
 import { NextRequest, NextResponse } from "next/server";
 import { google } from "@ai-sdk/google";
 import { auth } from "@clerk/nextjs/server";
+import { db } from "@/db/db";
+import { messageTable } from "@/db/schema/schema";
 
 export async function POST(request: NextRequest) {
   try {
-    const { isAuthenticated } = await auth();
+    const { isAuthenticated, userId } = await auth();
     if (!isAuthenticated) {
       return NextResponse.json({ error: "unauthorized" }, { status: 401 });
     }
@@ -13,6 +15,14 @@ export async function POST(request: NextRequest) {
     const result = streamText({
       model: google("gemini-2.5-flash"),
       messages: await convertToModelMessages(messages),
+      onFinish: async ({ text }) => {
+        await db.insert(messageTable).values({
+          type: "result",
+          role: "assistant",
+          content: text,
+          userId,
+        });
+      },
     });
     return result.toUIMessageStreamResponse();
   } catch (error) {
