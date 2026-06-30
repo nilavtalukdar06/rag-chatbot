7b388ee696b4efbc011307afa04b0fa67aee75c2
Web <webd75369@gmail.com>
2026-06-30 17:58:35 +0530

    added retrieval service

--- src/services/embeddings.ts
+++ src/services/embeddings.ts
@@ -3,6 +3,11 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 import { Document } from "@langchain/core/documents";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
+const embeddings = new OpenAIEmbeddings({
+  model: "text-embedding-3-large",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 export abstract class EmbeddingService {
   constructor() {}
   static async generateDocsEmbeddings(content: string, userId: string) {
@@ -17,10 +22,6 @@ export abstract class EmbeddingService {
       chunkOverlap: 200,
     });
     const chunks = await splitter.splitDocuments([document]);
-    const embeddings = new OpenAIEmbeddings({
-      model: "text-embedding-3-large",
-      apiKey: process.env.OPENAI_API_KEY,
-    });
     const vectorStore = await QdrantVectorStore.fromExistingCollection(
       embeddings,
       {
@@ -33,5 +34,25 @@ export abstract class EmbeddingService {
       success: true,
       message: "embeddings created and stored successfully",
     };
+  }
+  static async getVectorStore(query: string, userId: string) {
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+      embeddings,
+      {
+        url: process.env.QDRANT_URL,
+        collectionName: "rag-chatbot",
+      },
+    );
+    const context = await vectorStore.similaritySearch(query, 5, {
+      must: [
+        {
+          key: "metadata.userId",
+          match: {
+            value: userId,
+          },
+        },
+      ],
+    });
+    return context;
   }
 }
