a6d8d577580b102e4165505cb99a688af35a3c76
Web <webd75369@gmail.com>
2026-06-30 15:55:40 +0530

    increased file uplaod limit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   serverExternalPackages: ["pdf-parse", "pdfjs-dist"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
